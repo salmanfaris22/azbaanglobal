76801b738677ff9f14a9a4a220e41cf2b66fc1ba
Your Name <salman567633800@gmail.com>
2026-05-30 00:41:40 +0530

    number pushed

--- src/entities/location/model/locations.ts
+++ src/entities/location/model/locations.ts
@@ -14,6 +14,7 @@ export type Location = {
   address: string;
   phone1: string;
   phone2: string;
+    phone3?: string;
   directions: string;
 };
 
@@ -25,8 +26,9 @@ export const LOCATIONS: Location[] = [
     title:
       "Sheikha Mhara Building, Office No 218/16, 2nd Floor, Near Al Twar Center, Al Qusais, Dubai - UAE",
     address: "Al Tawhidi Building, Bur Dubai, UAE",
-    phone1: "+91 79027 77751",
-    phone2: "+91 79027 77721",
+    phone1: "+054 449 0155 ",
+    phone2: "+055 278 2255 ",
+    phone3: "+054 3692369 ",
     directions:
       "https://www.google.com/maps?q=Sheikha+Mhara+Building+Al+Twar+5+Dubai",
   },
@@ -36,8 +38,8 @@ export const LOCATIONS: Location[] = [
     map: "https://www.google.com/maps?q=Azbaan+Global+Attestation+%26+Apostille+Services+Kochi&output=embed",
     title: "OFFICE No-122,ANTRIKSH BHAWAN 22,K.G MARG,CONNAUGHT PLACE NEW DELHI-110001",
     address: "New Delhi, India",
-    phone1: "+91 79027 77751",
-    phone2: "+91 79027 77721",
+    phone1: "+91 790277 7751",
+    phone2: "+91 790233 4455",
     directions:
       "https://www.google.com/maps?q=Office+No-122+Antriksh+Bhawan+KG+Marg+New+Delhi",
   },
@@ -47,8 +49,8 @@ export const LOCATIONS: Location[] = [
     map: "https://www.google.com/maps?q=Azbaan+Global+Attestation+%26+Apostille+Services+Kochi&output=embed",
     title: "Kochi Office",
     address: "X77Q+F2W, Pallimukku, Kochi, Ernakulam, Kerala 682016",
-    phone1: "+91 79027 77751",
-    phone2: "+91 79027 77721",
+    phone1: "+91 79027 77740",
+    phone2: "+91 79023 34455",
     directions:
       "https://www.google.com/maps?q=Azbaan+Global+Attestation+%26+Apostille+Services+Kochi",
   },
@@ -59,8 +61,8 @@ export const LOCATIONS: Location[] = [
     title:
       "Room No. 63/3617, Ground, CD TOWER, New Bus Stand, EMS Stadium, Arayidathupalam, Kozhikode, Kerala 673004",
     address: "Kozhikode, Kerala",
-    phone1: "+91 79027 77751",
-    phone2: "+91 79027 77721",
+      phone1:  "+91 79027 78899",
+    phone2: "+91 79023 34455",
     directions: "https://www.google.com/maps?q=Azbaan+Tours+%26+Travels+Kozhikode",
   },
   {
